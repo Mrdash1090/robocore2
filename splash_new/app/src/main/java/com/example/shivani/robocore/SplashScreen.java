9682058edf5ab5d779fc1e1dc58fa787b6f1073d
Mrdash1090 <dnitesh22145@gmail.com>
2019-03-23 14:30:49 +0530

    Updated

--- splash_new/app/src/main/java/com/example/shivani/robocore/SplashScreen.java
+++ splash_new/app/src/main/java/com/example/shivani/robocore/SplashScreen.java
@@ -1,13 +1,25 @@
 package com.example.shivani.robocore;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SplashScreen extends AppCompatActivity {
 
+    private static int SPLASH_TIMER=2000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splashscreen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_TIMER);
     }
 }
